1dedb15746a28012fa977fabfe67f251c6735bfb
salicrupjuan <salicrupjuan@gmail.com>
2020-05-04 03:26:01 -0400

    homework submital

---
+++ VacationPy/config.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+api_key = "ac01de1af1eadec312036c1d5294ebd8"
+
+# google cloud API key
+gg_key = "AIzaSyB6W7VoidVIRGQA6bvdyEvZzSA7F1IkmrM"
---
+++ WeatherPy/config.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+api_key = "ac01de1af1eadec312036c1d5294ebd8"
